253f84285ac2f1bb57ae71201ccf16ac9ade5748
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-07-19 23:55:27 -0400

    Create Arithmescript.js

---
+++ Arithmescript.js
@@ -0,0 +1,37 @@
+var input = "";
+
+function add(number) {
+  input += number;
+}
+
+function out() {
+  alert(input);
+}
+
+function sub(number) {
+  input -= number;
+}
+
+function del() {
+  input = 0;
+}
+
+function set(value) {
+  input = value;
+}
+
+function mul(number) {
+  input *= number;
+}
+
+function div(number) {
+  input /= number;
+}
+
+function hw() {
+  input = "Hello, world!";
+}
+
+function v() {
+  input = 1.1;
+}
