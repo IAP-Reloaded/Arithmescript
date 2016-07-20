f7f5c06acf6a99c8228b20d11ab3ae4bfef9817a
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-07-19 23:58:09 -0400

    Create Arithmescript-oldest.js

---
+++ Arithmescript-oldest.js
@@ -0,0 +1,31 @@
+function add(number) {
+  input += number;
+}
+
+function out() {
+  alert(input);
+}
+
+function sub(number) {
+  input -= number;
+}
+
+function del() {
+  input = 0;
+}
+
+function set(value) {
+  input = value;
+}
+
+function mul(number) {
+  input *= number;
+}
+
+function div(number) {
+  input /= number;
+}
+
+function hw() {
+  input = "Hello, world!";
+}
